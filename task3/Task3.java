b750383f8a9e0c9a899586c319c8c56fddc00dd7
otttisss <otttisss@yandex.ru>
2023-08-04 18:09:34 +0300

    task3 started and json files added

--- task3/Task3.java
+++ task3/Task3.java
@@ -1,4 +1,14 @@
 package task3;
 
+import org.json.*;
+
 public class Task3 {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Incorrect number of args. Usage: java Task3.java args[0] args[1]");
+        }
+
+        String jsonFile = args[0];
+        String jsonFile2 = args[1];
+    }
 }
