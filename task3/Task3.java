cc1603a3adaf672c061c29d3ed822a386bfa9c45
otttisss <otttisss@yandex.ru>
2023-08-04 18:16:12 +0300

    task3 readFile method added

--- task3/Task3.java
+++ task3/Task3.java
@@ -2,6 +2,10 @@ package task3;
 
 import org.json.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Task3 {
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -10,5 +14,19 @@ public class Task3 {
 
         String jsonFile = args[0];
         String jsonFile2 = args[1];
+
+        try {
+            JSONObject testJson = readFile(jsonFile);
+            JSONObject valuesJson = readFile(jsonFile2);
+        } catch (JSONException e) {
+            System.err.println("Error JSON" + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Error with stream (read or write)" + e.getMessage());
+        }
+    }
+
+    public static JSONObject readFile(String fileName) throws JSONException, FileNotFoundException {
+        JSONTokener tokener = new JSONTokener(new FileReader(fileName));
+        return new JSONObject(tokener);
     }
 }
