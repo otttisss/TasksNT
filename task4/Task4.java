35df8124fdcbff03211ea78e4e307391b9c1dd41
otttisss <otttisss@yandex.ru>
2023-08-05 12:29:48 +0300

    task4 fileReader method done

--- task4/Task4.java
+++ task4/Task4.java
@@ -1,5 +1,11 @@
 package task4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task4 {
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -7,12 +13,33 @@ public class Task4 {
         }
 
         int[] nums = fileReader(args[0]);
-        int moves = findMinimum(nums);
-        System.out.println(moves);
+
+        if (nums != null) {
+            int moves = findMinimum(nums);
+            System.out.println(moves);
+        }
     }
 
-    private static int[] fileReader(String fileName) {
+    private static int[] fileReader(String fileName) { // Метод чтения из тестового файла
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            List<Integer> numsList = new ArrayList<>();
 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int num = Integer.parseInt(line);
+                numsList.add(num);
+            }
+
+            int[] nums = new int[numsList.size()];
+            for (int i = 0; i < numsList.size(); i++) {
+                nums[i] = numsList.get(i);
+            }
+
+            return nums;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private static int findMinimum(int[] nums) {
