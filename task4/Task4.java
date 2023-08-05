c6e94a7eaf89e2438019c32cd0e0f8345f1e0a19
otttisss <otttisss@yandex.ru>
2023-08-05 12:18:11 +0300

    task4 in progress

--- task4/Task4.java
+++ task4/Task4.java
@@ -1,5 +1,21 @@
 package task4;
 
 public class Task4 {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Incorrect number of args. Usage: java Task4.java test_file");
+        }
 
+        int[] nums = fileReader(args[0]);
+        int moves = findMinimum(nums);
+        System.out.println(moves);
+    }
+
+    private static int[] fileReader(String fileName) {
+
+    }
+
+    private static int findMinimum(int[] nums) {
+
+    }
 }
